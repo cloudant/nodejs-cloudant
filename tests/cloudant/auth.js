5459257fc87d7e9ea04c74bebb3fa757cb9b6581
Jason Smith (air) <jason@cloudant.com>
2014-09-17 07:12:42 +0700

    Remember both the key and password for the newly-created third party

--- tests/cloudant/auth.js
+++ tests/cloudant/auth.js
@@ -7,7 +7,7 @@ var specify  = require('specify')
 
 var mock = nock(helpers.cloudant_auth, "cloudant/auth");
 
-var third_party = null;
+var third_party = {key:null, password:null};
 
 specify('cloudant:generate_api_key', timeout, function (assert) {
   var client = Cloudant({account:'nodejs'}) // No password
@@ -25,7 +25,8 @@ specify('cloudant:generate_api_key', timeout, function (assert) {
     assert.ok(body.password   , 'Good API password');
 
     // Store this for later, to grant permissions to this third party.
-    third_party = body.key;
+    third_party.key = body.key;
+    third_party.password = body.password;
   });
 });
 
@@ -37,7 +38,7 @@ specify('cloudant:set_permissions', timeout, function (assert) {
   catch (er) { pw_error = er }
   assert.ok(pw_error, 'Throw an error if a password is not provided');
 
-  var opts = {database:'third_party_db', username:third_party, roles:['_reader','_writer']};
+  var opts = {database:'third_party_db', username:third_party.key, roles:['_reader','_writer']};
   client = Cloudant({account:'nodejs', password:helpers.cloudant_pw()})
   client.set_permissions(opts, function(er, body) {
     assert.equal(er, undefined, 'Hit the generate_api_key endpoint');
