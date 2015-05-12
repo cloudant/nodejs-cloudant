34b4662af067a6749b10d2484fafd66a4b26c83d
Glynn Bird <glynn.bird@gmail.com>
2015-05-12 17:09:25 +0100

    rework of the Cloudant library to use Nano as a simple dependency

--- cloudant.js
+++ cloudant.js
@@ -1,20 +1,30 @@
-module.exports = { reconfigure: reconfigure
-                 , fix_request     : fix_request
-                 , db_functions    : db_functions
-                 , server_functions: server_functions
-                 };
+var package = require('./package.json'),
+ nano = null;
+ 
+// https://docs.cloudant.com/api.html#creating-api-keys
+var generate_api_key = function(callback) {
+  nano.request( { path: "_api/v2/api_keys", method: "post" }, callback);
+};
 
-//
-// Cloudant functionality
-//
+// https://docs.cloudant.com/api.html#reading-the-cors-configuration
+var get_cors = function(callback) {
+  nano.request({ path: "_api/v2/user/config/cors" }, callback);
+};
 
-var _ = require('underscore');
-var URL = require('url');
+// https://docs.cloudant.com/api.html#setting-the-cors-configuration
+var set_cors = function(configuration, callback) {
+  nano.request( { path: "_api/v2/user/config/cors", method: "put", body: configuration }, callback);
+};
 
+// the /set_permissions API call is deprecated
+var set_permissions = function(opts, callback) {
+  console.error("set_permissions is deprecated. Please use set_security instead");
+  callback(null, null);
+};
 
-// Rebuild a standard Nano configuration object into one for use with Cloudant.
-function reconfigure(config) {
-  config = _.clone(config || {});
+// function from the old Cloudant library to parse an object { account: "myaccount", password: "mypassword"}
+var reconfigure = function(config) {
+  config = JSON.parse(JSON.stringify(config)); //clone
 
   // An account can be just the username, or the full cloudant URL.
   var match = config.account && config.account.match && config.account.match(/(\w+)\.cloudant\.com/);
@@ -30,188 +40,95 @@ function reconfigure(config) {
   else if (config.account)
     config.url = 'https://' + encodeURIComponent(config.account) + '.cloudant.com';
 
-  return config;
-}
+  return config.url;
+};
 
+// this IS the Cloudant library. It's nano + a few functions
+module.exports = function(credentials, couchdbcallback) {
+  
+  // keep a copy of the credentials
+  var requestDefaults = {}
+  if (typeof credentials == "object") {
+    if (credentials.requestDefaults) {
+      requestDefaults = credentials.requestDefaults;
+    }
+    credentials = reconfigure(credentials);
+  }
+  
+  // create a nano instance
+  nano = require('nano')({ url: credentials, requestDefaults: requestDefaults}, couchdbcallback || null);  
+  
+  // our own implementation of 'use' e.g. nano.use or nano.db.use
+  // it includes all db-level functions
+  var use = function(db) {
+    
+    // ****************
+    // Functions added to each db e.g. cloudant.use("mydb")
+    // ****************
+    
+    // https://docs.cloudant.com/api.html#viewing-permissions
+    var get_security = function(callback) {
+      nano.request( { path: "_api/v2/db/" + encodeURIComponent(db) + "/_security"}, callback);
+    };
 
-// Add the Cloudant API for server functions.
-function server_functions(nano) {
+    // https://docs.cloudant.com/api.html#modifying-permissions
+    var set_security = function(permissions, callback) {
+      nano.request( { path: "_api/v2/db/" + encodeURIComponent(db) + "/_security", method: "put", body: {cloudant: permissions} }, callback);
+    };
+    
+    // https://docs.cloudant.com/api.html#list-all-indexes &
+    // https://docs.cloudant.com/api.html#creating-a-new-index
+    var index = function(definition, callback) {
+      
+      // if no definition is provided, then the user wants see all the indexes
+      if (typeof definition == "function") {  
+        callback = definition;
+        nano.request({ path: encodeURIComponent(db) + "/_index"}, callback);
+      } else {
+        // the user wants to create a new index
+        nano.request({ path: encodeURIComponent(db) + "/_index", method:"post", body: definition}, callback);
+      }
+    };
+    
+    // https://docs.cloudant.com/api.html#deleting-an-index
+    var index_del = function(spec, callback) {
+      spec = spec || {}
+      if (!spec.ddoc)
+        throw new Error('index.del() must specify a "ddoc" value');
+      if (!spec.name)
+        throw new Error('index.del() must specify a "name" value');
+      var type = spec.type || 'json';     
+      var path = encodeURIComponent(db) + "/_index/" + encodeURIComponent(spec.ddoc) + "/" + encodeURIComponent(type) + "/" + encodeURIComponent(spec.name);
+      nano.request({ path:path, method:"delete"}, callback);
+    };
+    
+    // https://docs.cloudant.com/api.html#finding-documents-using-an-index
+    var find = function(query, callback) {
+      nano.request( { path: encodeURIComponent(db) + "/_find", method: "post", body: query}, callback);
+    };
+  
+    // add Cloudant special functions
+    var obj = nano._use(db);
+    obj.get_security = get_security;
+    obj.set_security = set_security;
+    obj.index = index;
+    obj.index.del = index_del;
+    obj.find = find;
+    
+    return obj;
+  };
+  
+  // intercept calls to 'nano.use' to plugin our extensions
+  nano._use = nano.use;
+  delete nano.use;
+  delete nano.db.use;
+  nano.use = nano.db.use = use;
+
+  // add top-level Cloudant-specific functions
   nano.generate_api_key = generate_api_key;
-  nano.set_permissions  = set_permissions;
-  nano.view_security = view_security;
-  nano.set_security = set_security;
-  nano.cors = cors;  
+  nano.get_cors = get_cors;
+  nano.set_cors = set_cors;
+  nano.set_permissions = set_permissions;
+  
   return nano;
-}
-
-
-// Modify a request just before it is about to go out. A valuable part of Nano
-// is its relax() function, chock full of fixes and workarounds to maneuver all
-// the idiosynchrasies of CouchDB and Cloudant. It is worth using. However, it
-// is not very extensible. In particular, it only allows querying paths inside
-// the main CouchDB URL. This is bad, because some Cloudant features are in a
-// completely different domain. Additionally, we do not want the maintenance
-// headache of modifying that function and then managing merge conflicts from
-// upstream changes. The solution is a one-line change to relax(), to call this
-// function immediately before the request runs. A faux method, CLOUDANT,
-// triggers special handling here, to do the right thing.
-function fix_request(req, config) {
-  if (req.method != 'CLOUDANT')
-    return;
-
-  var url = URL.parse(req.uri),
-    pathname = null,
-    db = null,
-    match = url.pathname.match(/\/(.*)(\/.*)$/); // extract /db/path
-  if (match) {
-    db = match[1];
-    pathname=match[2];
-  } else {
-    pathname = url.pathname
-  }
-
-  switch (pathname) {
-    
-    case '/generate_api_key':
-    
-      req.method = 'POST';
-      req.uri = 'https://' + encodeURIComponent(config.account) + ':' + encodeURIComponent(config.password) + '@cloudant.com/api/generate_api_key';
-      break;
-      
-    case '/set_permissions':
-      
-      req.method = 'POST';
-      req.uri = 'https://' + encodeURIComponent(config.account) + ':' + encodeURIComponent(config.password) + '@cloudant.com/api/set_permissions';
-      break;
-      
-    case '/view_security':
-      
-      req.method = 'GET';
-      req.uri = 'https://' + encodeURIComponent(config.account) + ':' + encodeURIComponent(config.password) + '@' + encodeURIComponent(config.account) + '.cloudant.com/_api/v2/db/' + encodeURIComponent(db) + '/_security';    
-      break;
-      
-    case '/set_security':
-      
-      req.method = 'PUT';
-      req.uri = 'https://' + encodeURIComponent(config.account) + ':' + encodeURIComponent(config.password) + '@' + encodeURIComponent(config.account) + '.cloudant.com/_api/v2/db/' + encodeURIComponent(db) + '/_security';
-      break;  
-    
-    case '/cors':
-      req.method = 'PUT';
-      req.uri = 'https://' + encodeURIComponent(config.account) + ':' + encodeURIComponent(config.password) + '@' + encodeURIComponent(config.account) + '.cloudant.com/_api/v2/user/config/cors';  
-      break;  
-    
-    default:
-    
-      throw new Error('Bad Cloudant request: ' + JSON.stringify(req));
-  }
-    
-}
-
-
-function generate_api_key(callback) {
-  var nano = this;
-
-  if (!nano.config.account)
-    throw new Error('generate_api_key requires an "account" parameter during Cloudant initialization');
-  if (!nano.config.password)
-    throw new Error('generate_api_key requires an "password" parameter during Cloudant initialization');
-
-  nano.relax({method:'CLOUDANT', path:'generate_api_key'}, callback);
-}
-
-function view_security(db, callback) {
-  var nano = this;
-  if(!db)
-    throw new Error('view_security requires a "db" parameter');
-  if (!nano.config.account)
-    throw new Error('view_security requires an "account" parameter during Cloudant initialization');
-  if (!nano.config.password)
-    throw new Error('view_security requires an "password" parameter during Cloudant initialization');
-
-  nano.relax({method:'CLOUDANT', path:'view_security', db:db }, callback);
-}
-
-function set_security(db, security, callback) {
-  var nano = this;
-  if(!db)
-    throw new Error('set_security requires a "db" parameter');
-  if(!security)
-    throw new Error('set_security requires a "security" parameter');
-  if (!nano.config.account)
-    throw new Error('set_security requires an "account" parameter during Cloudant initialization');
-  if (!nano.config.password)
-    throw new Error('set_security requires an "password" parameter during Cloudant initialization');
-
-  nano.relax({method:'CLOUDANT', path:'set_security', db:db , body:security}, callback);
-}
-
-
-function set_permissions(opts, callback) {
-  console.warn("DEPRECATED: set_permissions has been replaced by set_security");
-  var nano = this;
-
-  if (!nano.config.account)
-    throw new Error('set_permissions requires an "account" parameter during initialization');
-  if (!nano.config.password)
-    throw new Error('set_permissions requires an "password" parameter during initialization');
-
-  // Add the username prefix that Cloudant wants.
-  var db = opts.database || '';
-  opts.database = nano.config.account + '/' + db;
-
-  nano.relax({method:'CLOUDANT', path:'set_permissions', form:opts}, callback);
-}
-
-function cors(opts, callback) {
-  var nano = this;
-  
-  if (!nano.config.account)
-    throw new Error('cors requires an "account" parameter during initialization');
-  if (!nano.config.password)
-    throw new Error('cors requires an "password" parameter during initialization');
-  
-  nano.relax({method:'CLOUDANT', path:'cors', body: opts}, callback)
-}
-
-
-// Add the Cloudant API for database functions.
-function db_functions(db, relax) {
-  db.index     = function(def , cb) { db_index    (relax, db, def , cb) };
-  db.index.del = function(spec, cb) { index_delete(relax, db, spec, cb) };
-  db.find      = function(qry , cb) { index_find  (relax, db, qry , cb) };
-
-  return db;
-}
-
-function db_index(relax, db, definition, callback) {
-  if (!callback && typeof definition == 'function')
-    return index_list(relax, db, definition); // "definition" is in fact the callback.
-  else
-    return index_create(relax, db, definition, callback);
-}
-
-function index_list(relax, db, callback) {
-  return relax({db:db.config.db, path:'_index'}, callback);
-}
-
-function index_create(relax, db, definition, callback) {
-  return relax({method:'POST', db:db.config.db, path:'_index', body:definition}, callback);
-}
-
-function index_find(relax, db, query, callback) {
-  return relax({method:'POST', db:db.config.db, path:'_find', body:query}, callback);
-}
-
-function index_delete(relax, db, spec, callback) {
-  spec = spec || {}
-  if (!spec.ddoc)
-    throw new Error('index.del() must specify a "ddoc" value');
-  if (!spec.name)
-    throw new Error('index.del() must specify a "name" value');
-
-  var type = spec.type || 'json';
-  var path = '_index/' + spec.ddoc + '/' + type + '/' + spec.name;
-
-  return relax({method:'DELETE', db:db.config.db, path:path}, callback);
 }
