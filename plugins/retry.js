9f4d0b307fd2996577b08f101f436a09985dfb2e
Sam Smith <samsmith@linux.vnet.ibm.com>
2017-07-05 15:13:58 +0100

    eslint: fix plugins/retry.js

--- plugins/retry.js
+++ plugins/retry.js
@@ -1,16 +1,17 @@
-/**
- * Copyright (c) 2015 IBM Cloudant, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
- */
+// Copyright © 2017 IBM Corp. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict';
 
 // this the the 'retry' request handler.
 // If CouchDB/Cloudant responds with a 429 HTTP code
@@ -52,7 +53,7 @@ module.exports = function(options) {
         request(req, function(e, h, b) {
           done(null, [e, h, b]);
         }).on('response', function(r) {
-          statusCode = r && r.statusCode || 500;
+          statusCode = (r && r.statusCode) || 500;
         }).on('data', function(chunk) {
           if (statusCode !== 429) {
             s.write(chunk);
