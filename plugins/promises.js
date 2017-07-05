0030461c98ffc648c700a27bf47a87a84c64c5b2
Sam Smith <samsmith@linux.vnet.ibm.com>
2017-07-05 15:13:06 +0100

    eslint: fix plugins/promises.js

--- plugins/promises.js
+++ plugins/promises.js
@@ -1,16 +1,17 @@
-/**
- * Copyright (c) 2015 IBM Cloudant, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
- */
+// Copyright © 2017 IBM Corp. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict';
 
 // this the the 'promises' request handler.
 // It is a function that returns a Promise and resolves the promise on success
@@ -27,7 +28,7 @@ module.exports = function(options) {
     }
     return new Promise(function(resolve, reject) {
       request(req, function(err, h, b) {
-        var statusCode = h && h.statusCode || 500;
+        var statusCode = (h && h.statusCode) || 500;
         if (b) {
           try { b = JSON.parse(b); } catch (err) { }
         }
