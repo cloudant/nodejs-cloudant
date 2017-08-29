8180167409c428b57221be0aee69415b4bef2112
Sam Smith <samsmith@linux.vnet.ibm.com>
2017-08-29 07:59:51 +0100

    Add pipeData option to EventPipe

--- test/eventpipe.js
+++ test/eventpipe.js
@@ -86,4 +86,104 @@ describe('EventPipe', function() {
 
     ep.resume(); // note EventPipe starts in 'paused' mode
   });
+
+  it('pipes data from source to target', function(done) {
+    var source = new events.EventEmitter();
+    var target = new stream.PassThrough();
+    var ep = new EventPipe(source, target);
+
+    source.emit('request', {url: 'http://localhost:5986'});
+    source.emit('socket', {encrypted: true});
+    source.emit('response', {statusCode: 123});
+
+    // data
+    var sentData = ['foo', 'bar', 'baz'];
+    sentData.forEach(function(d) {
+      source.emit('data', d);
+    });
+
+    source.emit('end');
+
+    var data = [];
+    var seenRequestEvent = false;
+    var seenSocketEvent = false;
+
+    // add event handlers
+    target
+      .on('request', function(req) {
+        seenRequestEvent = true;
+        assert.equal(req.url, 'http://localhost:5986');
+      })
+      .on('socket', function(socket) {
+        seenSocketEvent = true;
+        assert.ok(socket.encrypted);
+      })
+      .on('pipe', function(src) {
+        assert.fail('Unexpected "pipe" event received.');
+      })
+      .on('data', function(d) {
+        data.push(d.toString('utf8'));
+      })
+      .on('end', function() {
+        assert.ok(seenRequestEvent);
+        assert.ok(seenSocketEvent);
+        assert.deepEqual(data, sentData);
+        done();
+      });
+
+    ep.resume(); // note EventPipe starts in 'paused' mode
+  });
+
+  it('pipes data from source to target (with `pipeData` enabled)', function(done) {
+    var source = new stream.Readable();
+
+    source._read = function() {}; // noop read
+
+    var target = new stream.PassThrough();
+    var ep = new EventPipe(source, target, true);
+
+    source.emit('request', {url: 'http://localhost:5986'});
+    source.emit('socket', {encrypted: true});
+    source.emit('response', {statusCode: 123});
+
+    // data
+    var sentData = ['foo', 'bar', 'baz'];
+    sentData.forEach(function(d) {
+      source.push(d);
+    });
+
+    source.push(null); // signal EOF
+
+    var data = [];
+    var seenRequestEvent = false;
+    var seenSocketEvent = false;
+    var seenPipeEvent = false;
+
+    // add event handlers
+    target
+      .on('request', function(req) {
+        seenRequestEvent = true;
+        assert.equal(req.url, 'http://localhost:5986');
+      })
+      .on('socket', function(socket) {
+        seenSocketEvent = true;
+        assert.ok(socket.encrypted);
+      })
+      .on('pipe', function(src) {
+        seenPipeEvent = true;
+        assert.equal(typeof src, 'object');
+      })
+      .on('data', function(d) {
+        data.push(d.toString('utf8'));
+      })
+      .on('end', function() {
+        assert.ok(seenRequestEvent);
+        assert.ok(seenSocketEvent);
+        assert.ok(seenPipeEvent);
+        assert.deepEqual(data, sentData);
+        done();
+      });
+
+    ep.resume(); // note EventPipe starts in 'paused' mode
+  });
 });
